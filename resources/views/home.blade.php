b873942e8d57ab7f7fc4e8da8d44c4fe5461b695
Cix8 <francesco.natal_fn@libero.it>
2022-06-29 15:20:18 +0200

    add PageController, add home_route, print trains_data in view_home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
+
 <body>
-    
+
     <h1>Homepage</h1>
 
+    <h2>Trains</h2>
+
+    <ol>
+        @foreach ($trains as $train)
+            <li>
+                <p><strong>Agenzia:</strong> {{ ucfirst($train->agency) }}</p>
+                <p><strong>Stazione di partenza:</strong> {{ $train->departure_station }}</p>
+                <p><strong>Stazione di arrivo:</strong> {{ $train->arrival_station }}</p>
+                <p><strong>Data/ora partenza:</strong> {{ $train->departure_time }}</p>
+                <p><strong>Data/ora arrivo:</strong> {{ $train->arrival_time }}</p>
+                <p><strong>Codice: </strong> {{ $train->train_code }}</p>
+                <p><strong>Numero di carrozze:</strong> {{ $train->carriages_number }}</p>
+                <p><strong>In orario:</strong> {{ $train->on_time == true ? 'Si' : 'No' }}</p>
+                <p><strong>Cancellato:</strong> {{ $train->deleted == true ? 'Si' : 'No' }}</p>
+            </li>
+        @endforeach
+    </ol>
+
 </body>
+
 </html>
